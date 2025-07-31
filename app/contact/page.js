6cfb35f044eff0d3e376d0ec7d99c7111c74ae04
Laura Turjman <contact@lauraturjman.fr>
2025-07-31 15:12:39 +0200

    remove map and add text to contactpage

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,19 +1,30 @@
 import ContactForm from "@/components/ContactForm";
-import dynamic from "next/dynamic";
-
-const MapClient = dynamic(() => import("./MapClient"), {
-  ssr: false,
-});
 
 export default function Contact() {
   return (
-    <div className="max-w-6xl mx-auto px-4 py-12">
-      <h1 className="text-3xl font-bold mb-8 text-center">Contact</h1>
+    <div className="flex max-w-4xl mx-auto px-4 h-screen justify-center items-center">
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 items-start">
+        <div>
+          <h2 className="text-3xl font-bold text-right">Contactez-Nous</h2>
+          <div className="mt-6 text-right">
+            <span>
+              Spline Studio est une société de production audiovisuelle créative
+              qui existe depuis 2016.
+            </span>
+
+            <div className="mt-6 font-semibold text-right">
+              Nous ne prenons ni stagiaires, ni alternants pour le moment.
+            </div>
+
+            <div className="mt-6 font-bold">Spline Studio</div>
+            <div>86 Boulevard de la Croix Rousse</div>
+            <div>69004 LYON</div>
+          </div>
+        </div>
+
         <div>
           <ContactForm />
         </div>
-        <MapClient />
       </div>
     </div>
   );
