ee06ebcdcf82c572f44596e529895d617c95ac61
Laura Turjman <contact@lauraturjman.fr>
2025-07-31 14:11:55 +0200

    hide map

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,16 +1,16 @@
 // import ContactMap from "@/components/ContactMap";
-// import ContactForm from "@/components/ContactForm";
+import ContactForm from "@/components/ContactForm";
 
 export default function Contact() {
   return (
     <div className="max-w-6xl mx-auto px-4 py-12">
       <h1 className="text-3xl font-bold mb-8 text-center">Contact</h1>
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 items-start">
-        {/* <div>
+        <div>
           <ContactForm />
         </div>
 
-        <ContactMap /> */}
+        {/* <ContactMap /> */}
       </div>
     </div>
   );
