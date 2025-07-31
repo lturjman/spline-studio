dde25ae3168556d5aed59b41e42897f8784c8954
Laura Turjman <contact@lauraturjman.fr>
2025-07-31 11:13:24 +0200

    fix map

--- app/contact/Map.js
+++ app/contact/Map.js
@@ -1,8 +1,17 @@
 "use client";
-import Map from "@/components/Maps";
-import { motion } from "motion/react";
 
-const RECAPTCHA_KEY = "6LezkpErAAAAAJOoz0ea_1wKT1fpzWuMCJbnggQa";
+import { motion } from "motion/react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+const customIcon = new L.Icon({
+  iconUrl: "/pin.png",
+  iconSize: [30],
+  iconAnchor: [15, 30],
+  popupAnchor: [0, -30],
+});
+const position = [45.7736, 4.8256];
 
 export default function ContactMap() {
   return (
@@ -13,7 +22,25 @@ export default function ContactMap() {
       viewport={{ once: true }}
       className="rounded-2xl overflow-hidden shadow-lg w-full max-w-xl mx-auto"
     >
-      <Map />
+      <div className="w-full h-[60vh] rounded-2xl overflow-hidden shadow-md">
+        <MapContainer
+          center={position}
+          zoom={14}
+          scrollWheelZoom={true}
+          dragging={true}
+          doubleClickZoom={true}
+          zoomControl={true}
+          className="h-full w-full grayscale-[10%]"
+        >
+          <TileLayer
+            url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+            attribution='&copy; <a href="https://carto.com/">CARTO</a>'
+          />
+          <Marker position={position} icon={customIcon}>
+            <Popup>Spline Studio</Popup>
+          </Marker>
+        </MapContainer>
+      </div>
     </motion.div>
   );
 }
