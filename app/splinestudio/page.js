9d02734e355e01bd1b06840f923e20ed9712b55a
Laura Turjman <contact@lauraturjman.fr>
2025-08-08 18:35:07 +0200

    connect sline studio page to prismic

--- app/splinestudio/page.js
+++ app/splinestudio/page.js
@@ -1,7 +1,14 @@
-export default function SplineStudio() {
+import { createClient } from "@/prismicio";
+
+export default async function SplineStudio() {
+  const client = createClient();
+  const page = await client.getSingle("spline_studio");
+
   return (
     <div>
-      <h1>Spline Studio</h1>
+      <h1 className="mt-10 font-bold m-auto w-full text-center font-archivoBlack uppercase tracking-tight text-4xl">
+        {page.data.title}
+      </h1>
     </div>
   );
 }
