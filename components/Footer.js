890d61db65711c7efd12caea4204a01f5ba17f0c
Laura Turjman <contact@lauraturjman.fr>
2025-08-21 11:11:16 +0200

    add mentions on footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,53 +1,57 @@
 import Logo from "@/components/DynamicLogo";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="bg-black p-8 pb-10 relative flex flex-col md:flex-row items-start justify-between w-full">
+    <footer className="bg-black p-8 pb-14 relative flex flex-col md:flex-row items-start justify-between w-full">
       {/* Left column */}
       <div className="flex flex-col text-white uppercase tracking-tight text-lg z-10">
-        <a href="#" className="hover:text-emerald-300">
+        <Link href="#" className="hover:text-emerald-300">
           Accueil
-        </a>
-        <a href="/splinestudio" className="hover:text-emerald-300">
+        </Link>
+        <Link href="/splinestudio" className="hover:text-emerald-300">
           Spline Studio
-        </a>
-        <a href="/films" className="hover:text-emerald-300">
+        </Link>
+        <Link href="/films" className="hover:text-emerald-300">
           Films
-        </a>
+        </Link>
         <div className=" uppercase tracking-tight text-sm pt-4">
-          <a
+          <Link
             href="https://www.instagram.com/splinestd/"
             className="mr-4 hover:text-emerald-300"
           >
             Instagram
-          </a>
-          <a
+          </Link>
+          <Link
             href="https://www.linkedin.com/company/splinestudio/"
             className="hover:text-emerald-300"
           >
             LinkedIn
-          </a>
+          </Link>
         </div>
       </div>
 
       {/* Right column */}
       <div className="flex flex-col text-white text-right text-lg uppercase tracking-tight ml-auto z-10">
-        <a href="/contact" className="hover:text-emerald-300 ">
+        <Link href="/contact" className="hover:text-emerald-300 ">
           Contact
-        </a>
-        <a href="/mentionslegales" className="text-sm hover:text-emerald-300">
+        </Link>
+        <Link
+          href="/mentionslegales"
+          className="text-sm hover:text-emerald-300"
+        >
           Mentions légales
-        </a>
+        </Link>
       </div>
 
       {/* Animated GIF Logo */}
       <div
         className="
-    z-0 h-10 flex justify-center items-center overflow-hidden 
-    w-[200px] md:w-[300px]
-    md:absolute md:left-1/2 md:top-1/2 md:translate-x-[-50%] md:translate-y-[-50%]
-  "
+          z-0 h-10 flex justify-center items-center overflow-hidden 
+          w-[200px] md:w-[300px]
+          md:absolute md:left-1/2 md:top-1/2 md:translate-x-[-50%] md:translate-y-[-50%]
+        "
       >
         <video
           src="/GIFSplineStudio.webm"
@@ -58,6 +62,11 @@ export default function Footer() {
           playsInline
           aria-label="Logo animé Spline Studio"
         />
+      </div>
+
+      {/* Mention en bas centré */}
+      <div className="absolute bottom-2 left-1/2 -translate-x-1/2 text-gray-600 text-xs text-center">
+        © 2025 - SPLINE Studio – Tous droits réservés
       </div>
     </footer>
   );
